b2931ddf853be3901ca57ec2310c46da392ac508
tosimb <tosimb@yandex.ru>
2024-05-12 23:04:02 +0300

    v2.4.5 Подключил шаблон для html

--- src/gui/router.py
+++ src/gui/router.py
@@ -1,15 +1,34 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from deps import get_db_repo
 
 from logger.logger import logger
 
-
+templates = Jinja2Templates(directory="templates")
 
 router = APIRouter(
     prefix="/gui",
     tags=["Gui"]
 )
 
+@router.get("", response_class=HTMLResponse)
+async def gui_pages(request: Request, db=Depends(get_db_repo)):
+    """
+    GUI
+    """
+    dump = db.select_last_agents_reg("gui", 20)
+    vvk = None
+    agents = None
+    if dump:
+        vvk = [item for item in dump if item.get("type_id") == False]
+        agents = [item for item in dump if item.get("type_id") == True]
+        vvk=vvk[0]
+        agents.sort(key=lambda x: x.get("id"))
+
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"vvk": vvk, "agents": agents}
+    )
 
 @router.get("/all_vvk_scheme")
 async def all_vvk_scheme(db=Depends(get_db_repo)):
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
-from postgres import connect, disconnect
 
+from postgres import connect, disconnect
 from params.router import router as router_params
 from registration.router import router as router_registration
 from gui.router import router as router_gui
@@ -25,6 +26,8 @@ def get_application() -> FastAPI:
 
 app = get_application() 
 app.add_middleware(LogMiddleware)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 app.include_router(router_params)
 app.include_router(router_registration)
